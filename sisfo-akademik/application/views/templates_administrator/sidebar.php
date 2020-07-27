94b6ad9fb66e665b1ccb77eabc4b4add3e8a542c
dzulfikriAlfik <fikriallfik@gmail.com>
2020-07-27 21:39:29 +0700

    menambahkan file view untuk templating

--- sisfo-akademik/application/views/templates_administrator/sidebar.php
+++ sisfo-akademik/application/views/templates_administrator/sidebar.php
@@ -8,10 +8,10 @@
 
          <!-- Sidebar - Brand -->
          <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-            <div class="sidebar-brand-icon rotate-n-15">
-               <i class="fas fa-laugh-wink"></i>
+            <div class="sidebar-brand-icon">
+               <i class="fas fa-university"></i>
             </div>
-            <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+            <div class="sidebar-brand-text mx-1">Sisfo Akademik</div>
          </a>
 
          <!-- Divider -->
@@ -24,25 +24,25 @@
                <span>Dashboard</span></a>
          </li>
 
-         <!-- Divider -->
-         <hr class="sidebar-divider">
-
-         <!-- Heading -->
-         <div class="sidebar-heading">
-            Interface
-         </div>
-
          <!-- Nav Item - Pages Collapse Menu -->
          <li class="nav-item">
             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-               <i class="fas fa-fw fa-cog"></i>
-               <span>Components</span>
+               <i class="fas fa-fw fa-university"></i>
+               <span>Akademik</span>
             </a>
             <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                <div class="bg-white py-2 collapse-inner rounded">
-                  <h6 class="collapse-header">Custom Components:</h6>
-                  <a class="collapse-item" href="buttons.html">Buttons</a>
-                  <a class="collapse-item" href="cards.html">Cards</a>
+                  <h6 class="collapse-header">Sub-Menu Akademik:</h6>
+                  <a class="collapse-item" href="buttons.html">Jurusan</a>
+                  <a class="collapse-item" href="cards.html">Program Studi</a>
+                  <a class="collapse-item" href="cards.html">Mata Kuliah</a>
+                  <a class="collapse-item" href="cards.html">Mahasiswa</a>
+                  <a class="collapse-item" href="cards.html">Tahun Akademik</a>
+                  <a class="collapse-item" href="cards.html">KRS</a>
+                  <a class="collapse-item" href="cards.html">Input Nilai</a>
+                  <a class="collapse-item" href="cards.html">KHS</a>
+                  <a class="collapse-item" href="cards.html">Cetak Transkrip</a>
+                  <a class="collapse-item" href="cards.html">Dosen</a>
                </div>
             </div>
          </li>
@@ -51,43 +51,34 @@
          <li class="nav-item">
             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
                <i class="fas fa-fw fa-wrench"></i>
-               <span>Utilities</span>
+               <span>Pengaturan</span>
             </a>
             <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
                <div class="bg-white py-2 collapse-inner rounded">
-                  <h6 class="collapse-header">Custom Utilities:</h6>
-                  <a class="collapse-item" href="utilities-color.html">Colors</a>
-                  <a class="collapse-item" href="utilities-border.html">Borders</a>
-                  <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                  <a class="collapse-item" href="utilities-other.html">Other</a>
+                  <h6 class="collapse-header">Sub-Menu Pengaturan:</h6>
+                  <a class="collapse-item" href="utilities-color.html">User</a>
+                  <a class="collapse-item" href="utilities-border.html">Menu</a>
                </div>
             </div>
          </li>
-
-         <!-- Divider -->
-         <hr class="sidebar-divider">
-
-         <!-- Heading -->
-         <div class="sidebar-heading">
-            Addons
-         </div>
 
          <!-- Nav Item - Pages Collapse Menu -->
          <li class="nav-item">
             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
                <i class="fas fa-fw fa-folder"></i>
-               <span>Pages</span>
+               <span>Info Kampus</span>
             </a>
             <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
                <div class="bg-white py-2 collapse-inner rounded">
-                  <h6 class="collapse-header">Login Screens:</h6>
-                  <a class="collapse-item" href="login.html">Login</a>
-                  <a class="collapse-item" href="register.html">Register</a>
-                  <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                  <div class="collapse-divider"></div>
-                  <h6 class="collapse-header">Other Pages:</h6>
-                  <a class="collapse-item" href="404.html">404 Page</a>
-                  <a class="collapse-item" href="blank.html">Blank Page</a>
+                  <h6 class="collapse-header">Sub-Menu Info Kampus:</h6>
+                  <a class="collapse-item" href="login.html">Identitas</a>
+                  <a class="collapse-item" href="register.html">Kategori</a>
+                  <a class="collapse-item" href="forgot-password.html">Informasi Kampus</a>
+                  <a class="collapse-item" href="404.html">Tentang kampus</a>
+                  <a class="collapse-item" href="blank.html">Fasilitas</a>
+                  <a class="collapse-item" href="blank.html">Materi Perkuliahan</a>
+                  <a class="collapse-item" href="blank.html">Gallery</a>
+                  <a class="collapse-item" href="blank.html">Kontak</a>
                </div>
             </div>
          </li>
@@ -95,15 +86,8 @@
          <!-- Nav Item - Charts -->
          <li class="nav-item">
             <a class="nav-link" href="charts.html">
-               <i class="fas fa-fw fa-chart-area"></i>
-               <span>Charts</span></a>
-         </li>
-
-         <!-- Nav Item - Tables -->
-         <li class="nav-item">
-            <a class="nav-link" href="tables.html">
-               <i class="fas fa-fw fa-table"></i>
-               <span>Tables</span></a>
+               <i class="fas fa-fw fa-sign-out-alt"></i>
+               <span>Logout</span></a>
          </li>
 
          <!-- Divider -->
