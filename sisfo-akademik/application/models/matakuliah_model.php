94ff42ad780b8f77dadcf932831936564fd34cda
dzulfikriAlfik <fikriallfik@gmail.com>
2020-07-29 07:41:56 +0700

    menambahkan fungsi tambah data mata kuliah

--- sisfo-akademik/application/models/matakuliah_model.php
+++ sisfo-akademik/application/models/matakuliah_model.php
@@ -7,4 +7,9 @@ class Matakuliah_model extends CI_Model
    {
       return $this->db->get($table);
    }
+
+   public function insert_data($data, $table)
+   {
+      $this->db->insert($table, $data);
+   }
 }
