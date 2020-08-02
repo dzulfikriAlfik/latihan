5ab39caaa9a882363bd40ccbda5831093de15191
dzulfikriAlfik <fikriallfik@gmail.com>
2020-08-02 15:46:16 +0700

    mengubah link side bar untuk Input Nilai

--- sisfo-akademik/application/views/templates_administrator/sidebar.php
+++ sisfo-akademik/application/views/templates_administrator/sidebar.php
@@ -39,7 +39,7 @@
                   <a class="collapse-item" href="<?= base_url('administrator/mahasiswa'); ?>">Mahasiswa</a>
                   <a class="collapse-item" href="<?= base_url('administrator/tahun_akademik'); ?>">Tahun Akademik</a>
                   <a class="collapse-item" href="<?= base_url('administrator/krs'); ?>">KRS</a>
-                  <a class="collapse-item" href="#">Input Nilai</a>
+                  <a class="collapse-item" href="<?= base_url('administrator/nilai'); ?>">Input Nilai</a>
                   <a class="collapse-item" href="<?= base_url('administrator/khs'); ?>">KHS</a>
                   <a class="collapse-item" href="#">Cetak Transkrip</a>
                   <a class="collapse-item" href="#">Dosen</a>
