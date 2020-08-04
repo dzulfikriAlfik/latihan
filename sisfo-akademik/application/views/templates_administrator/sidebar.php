424bc8551c9c6ab8fd83ea5bfb99f5a1ecd86faf
dzulfikriAlfik <fikriallfik@gmail.com>
2020-08-04 13:54:28 +0700

    mengubah link sidebar untuk menu users

--- sisfo-akademik/application/views/templates_administrator/sidebar.php
+++ sisfo-akademik/application/views/templates_administrator/sidebar.php
@@ -56,7 +56,7 @@
             <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
                <div class="bg-white py-2 collapse-inner rounded">
                   <h6 class="collapse-header">Sub-Menu Pengaturan:</h6>
-                  <a class="collapse-item" href="utilities-color.html">User</a>
+                  <a class="collapse-item" href="<?= base_url('administrator/user'); ?>">User</a>
                   <a class="collapse-item" href="utilities-border.html">Menu</a>
                </div>
             </div>
