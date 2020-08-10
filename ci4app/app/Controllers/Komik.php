c5edbf1890c8d6927995c2fab0a8e0d23c3bd72b
dzulfikriAlfik <fikriallfik@gmail.com>
2020-08-10 07:27:53 +0700

    menambahkan file baru dan mengubah file berdasarkan video tutorial WPU seri ke 7 tentang Model

--- ci4app/app/Controllers/Komik.php
+++ ci4app/app/Controllers/Komik.php
@@ -2,9 +2,35 @@
 
 namespace App\Controllers;
 
-class Pages extends BaseController
+use App\Models\KomikModel;
+
+class Komik extends BaseController
 {
+   protected $komikModel;
+   public function __construct()
+   {
+      $this->komikModel = new KomikModel();
+   }
 
-   
 
+   public function index()
+   {
+      $komik = $this->komikModel->findAll();
+
+      $data = [
+         'title'  => 'Daftar Komik',
+         'komik'  => $komik
+      ];
+
+      // cara konek db tanpa model
+      // $db = \Config\Database::connect();
+      // $komik = $db->query("SELECT * FROM komik");
+      // foreach($komik->getResultArray() as $row){
+      //    d($row);
+      // }
+
+      // $KomikModel = new \App\Models\KomikModel();
+
+      return view('komik/index', $data);
+   }
 }
