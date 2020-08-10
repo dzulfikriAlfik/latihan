2817a7a79f9088d5ec41865f1712979defe0dd0f
dzulfikriAlfik <fikriallfik@gmail.com>
2020-08-10 08:50:15 +0700

    video tutorial WPU CI4 seri ke 8 ttg Model bagian 2

--- ci4app/app/Controllers/Komik.php
+++ ci4app/app/Controllers/Komik.php
@@ -15,11 +15,11 @@ class Komik extends BaseController
 
    public function index()
    {
-      $komik = $this->komikModel->findAll();
+      // $komik = $this->komikModel->findAll();
 
       $data = [
          'title'  => 'Daftar Komik',
-         'komik'  => $komik
+         'komik'  => $this->komikModel->getKomik()
       ];
 
       // cara konek db tanpa model
@@ -34,8 +34,12 @@ class Komik extends BaseController
       return view('komik/index', $data);
    }
 
-   public function detail($slug) 
+   public function detail($slug)
    {
-      echo $slug;
+      $data = [
+         'title'  => 'Detail Komik',
+         'komik'  => $this->komikModel->getKomik($slug)
+      ];
+      return view('komik/detail', $data);
    }
 }
