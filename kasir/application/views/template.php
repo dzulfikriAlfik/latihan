d640a226fbdc6fdab99206a882e5307a8341d69a
dzulfikriAlfik <fikriallfik@gmail.com>
2020-08-11 16:14:35 +0700

    Modifikasi : application/views/template.php -> mengubah link

--- kasir/application/views/template.php
+++ kasir/application/views/template.php
@@ -45,7 +45,7 @@
       <!-- Main Sidebar Container -->
       <aside class="main-sidebar sidebar-dark-primary elevation-4">
          <!-- Brand Logo -->
-         <a href="<?= base_url('assets/'); ?>/index3.html" class="brand-link">
+         <a href="<?= base_url('dashboard'); ?>" class="brand-link">
             <img src="<?= base_url('assets/'); ?>/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">My Kasir</span>
          </a>
@@ -68,7 +68,7 @@
                   <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
                   <li class="nav-item">
-                     <a href="../widgets.html" class="nav-link">
+                     <a href="<?= base_url('dashboard'); ?>" class="nav-link">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                            Dashboard
@@ -76,7 +76,7 @@
                      </a>
                   </li>
                   <li class="nav-item">
-                     <a href="../widgets.html" class="nav-link">
+                     <a href="<?= base_url(''); ?>" class="nav-link">
                         <i class="nav-icon fas fa-truck"></i>
                         <p>
                            Suppliers
@@ -84,7 +84,7 @@
                      </a>
                   </li>
                   <li class="nav-item">
-                     <a href="../widgets.html" class="nav-link">
+                     <a href="<?= base_url(''); ?>" class="nav-link">
                         <i class="nav-icon fas fa-users"></i>
                         <p>
                            Customers
