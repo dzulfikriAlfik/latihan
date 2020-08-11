bae81d4303fcf007f2986e150d198b50ede7e7f5
dzulfikriAlfik <fikriallfik@gmail.com>
2020-08-11 13:52:27 +0700

    Modifikasi : 'application/controllers/Auth.php -> menambahkan fungsi login' & 'application/models/User_model.php -> menambahkan pengecekan ke database untuk username dan password'

--- kasir/application/controllers/Auth.php
+++ kasir/application/controllers/Auth.php
@@ -3,6 +3,13 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
+
+   public function __construct()
+   {
+      parent::__construct();
+      $this->load->model('User_model');
+   }
+
    // BEGIN Login
    public function login()
    {
@@ -15,7 +22,28 @@ class Auth extends CI_Controller
    // BEGIN Process
    public function process()
    {
-      echo 'Proses';
+      $post = $this->input->post(null, TRUE);
+
+      if (isset($post['login'])) {
+         $query = $this->User_model->login($post);
+         if ($query->num_rows() > 0) {
+            $row     = $query->row();
+            $params  = [
+               'userid' => $row->user_id,
+               'level'  => $row->level
+            ];
+            $this->session->set_userdata($params);
+            echo "<script>
+                     alert('Login Berhasil!');
+                     window.location='" . site_url('dashboard') . "';
+                  </script>";
+         } else {
+            echo "<script>
+                     alert('Login Gagal! Username/Password Salah!');
+                     window.location='" . site_url('auth/login') . "';
+                  </script>";
+         }
+      }
    }
    // END
 }
