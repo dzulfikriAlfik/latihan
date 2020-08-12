adc883296751d6cb4374da913dff565d46c59d2c
dzulfikriAlfik <fikriallfik@gmail.com>
2020-08-12 21:07:12 +0700

    Modifikasi : 'data berhasil ditambahkan ke database'

--- kasir/application/controllers/User.php
+++ kasir/application/controllers/User.php
@@ -7,13 +7,13 @@ class User extends CI_Controller
    public function __construct()
    {
       parent::__construct();
-      $this->load->model('User_model');
+      $this->load->model('user_model');
       cek_not_login();
    }
 
    public function index()
    {
-      $data['row'] = $this->User_model->get()->result_array();
+      $data['row'] = $this->user_model->get()->result_array();
 
       $this->template->load('template', 'user/user_data', $data);
    }
@@ -29,6 +29,9 @@ class User extends CI_Controller
       $this->form_validation->set_rules('level', 'Level', 'required');
       $this->form_validation->set_message('required', '%s Harus diisi!');
       $this->form_validation->set_message('min_length', '{field} harus diisi minimal {param} karakter.');
+      $this->form_validation->set_message('is_unique', '%s sudah ada di database.');
+      // error delimiters
+      $this->form_validation->set_error_delimiters('<span class="text-red small">', '</span>');
    }
 
    public function add()
@@ -38,7 +41,13 @@ class User extends CI_Controller
       if ($this->form_validation->run() == FALSE) {
          $this->template->load('template', 'user/user_form_add');
       } else {
-         echo "Kontol";
+         $post = $this->input->post(null, TRUE);
+         $this->user_model->add($post);
+
+         if ($this->db->affected_rows() > 0) {
+            $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">Data User Berhasil ditambahkan<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
+            redirect('user');
+         }
       }
    }
 }
