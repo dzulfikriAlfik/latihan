631eda9eebe66bf9969eda8f0df645c6c724e6b6
dzulfikriAlfik <fikriallfik@gmail.com>
2020-08-12 21:09:19 +0700

    Modifikasi : 'mengubah format penampilan form_error'

--- kasir/application/views/user/user_form_add.php
+++ kasir/application/views/user/user_form_add.php
@@ -30,37 +30,37 @@
                         <div class="form-group">
                            <label for="fullname">Name <span class="text-red">*</span></label>
                            <input type="text" name="fullname" id="fullname" class="form-control" placeholder="Your Full Name" value="<?= set_value('fullname'); ?>">
-                           <?= form_error('fullname', '<div class="small text-danger">', '</div>'); ?>
+                           <?= form_error('fullname'); ?>
                         </div>
                         <div class="form-group">
                            <label for="username">Username <span class="text-red">*</span></label>
                            <input type="text" name="username" id="username" class="form-control" placeholder="Your Username" value="<?= set_value('username'); ?>">
-                           <?= form_error('username', '<div class="small text-danger">', '</div>'); ?>
+                           <?= form_error('username'); ?>
                         </div>
                         <div class="form-group">
                            <label for="password">Password <span class="text-red">*</span></label>
                            <input type="password" name="password" id="password" class="form-control" placeholder="Your Password">
-                           <?= form_error('password', '<div class="small text-danger">', '</div>'); ?>
+                           <?= form_error('password'); ?>
                         </div>
                         <div class="form-group">
                            <label for="passconf">Password Confirmation <span class="text-red">*</span></label>
                            <input type="password" name="passconf" id="passconf" class="form-control" placeholder="Password Confirmation">
-                           <?= form_error('passconf', '<div class="small text-danger">', '</div>'); ?>
+                           <?= form_error('passconf'); ?>
                         </div>
                      </div>
                      <div class="col-md-5 mx-3">
                         <div class="form-group">
                            <label for="address">Address</label>
-                           <textarea name="address" id="address" cols="30" rows="4" class="form-control"></textarea>
+                           <textarea name="address" id="address" cols="30" rows="4" class="form-control"><?= set_value('address'); ?></textarea>
                         </div>
                         <div class="form-group">
                            <label for="level">Level <span class="text-red">*</span></label>
                            <select name="level" id="level" class="form-control">
                               <option value="">-- Select Level --</option>
-                              <option value="1">Admin</option>
-                              <option value="2">Kasir</option>
+                              <option value="1" <?= set_value('level') == 1 ? "selected" : null; ?>>Admin</option>
+                              <option value="2" <?= set_value('level') == 2 ? "selected" : null; ?>>Kasir</option>
                            </select>
-                           <?= form_error('level', '<div class="small text-danger">', '</div>'); ?>
+                           <?= form_error('level'); ?>
                         </div>
                         <div class="form-group">
                            <button type="submit" class="btn btn-success btn-flat"><i class="fas fa-paper-plane"></i> Save</button>
