d8d534851db72a7a9b3c8d827740d003c0d2f571
dzulfikriAlfik <fikriallfik@gmail.com>
2020-08-12 20:02:26 +0700

    Modifikasi -> 'mengubah beberapa baris bagian button dll'

--- kasir/application/views/user/user_data.php
+++ kasir/application/views/user/user_data.php
@@ -2,7 +2,7 @@
    <div class="container-fluid">
       <div class="row mb-2">
          <div class="col-sm-6">
-            <h1>Daftar Users</h1>
+            <h1>Users</h1>
          </div>
          <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
@@ -21,23 +21,14 @@
       <div class="col-12">
          <div class="card">
             <div class="card-header">
-               <a href="<?= base_url('user/add'); ?>" class="btn btn-info btn-sm"><i class="fas fa-user-plus"></i> Tambah Data</a>
-               <div class="card-tools">
-                  <div class="input-group input-group-sm" style="width: 150px;">
-                     <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-
-                     <div class="input-group-append">
-                        <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
-                     </div>
-                  </div>
-               </div>
+               <a href="<?= base_url('user/add'); ?>" class="btn btn-info btn-sm"><i class="fas fa-user-plus"></i> Add User</a>
             </div>
             <!-- /.card-header -->
             <div class="card-body table-responsive p-0" style="height: 300px;">
-               <table class="table table-head-fixed text-nowrap table-hover">
-                  <thead>
+               <table class="table table-head-fixed table-bordered text-nowrap table-hover">
+                  <thead class="text-center">
                      <tr>
-                        <th>#</th>
+                        <th width="50px">No.</th>
                         <th>Username</th>
                         <th>Name</th>
                         <th>Address</th>
@@ -55,7 +46,7 @@
                            <td><?= $user['name']; ?></td>
                            <td><?= $user['address']; ?></td>
                            <td><?= $user['level'] == 1 ? "Admin" : "Kasir"; ?></td>
-                           <td>
+                           <td width="160px">
                               <a href="<?= base_url('user/edit/') . $user['user_id']; ?>" class="btn btn-warning btn-xs"><i class="fas fa-edit"></i> Edit</a>&nbsp;
                               <a href="<?= base_url('user/hapus/') . $user['user_id']; ?>" onclick="return confirm('Yakin Hapus Data?')" class="btn btn-danger btn-xs"><i class="fas fa-trash"></i> Hapus</a>
                            </td>
