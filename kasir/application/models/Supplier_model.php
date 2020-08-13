4674536caa440fcd1dac54cd613f53e23d28ed06
dzulfikriAlfik <fikriallfik@gmail.com>
2020-08-13 21:52:55 +0700

    Modifikasi : 'menambahkan method add dan edit data supplier'

--- kasir/application/models/Supplier_model.php
+++ kasir/application/models/Supplier_model.php
@@ -13,6 +13,30 @@ class Supplier_model extends CI_Model
       return $this->db->get();
    }
 
+   public function add($post)
+   {
+      $params = [
+         'name'         => $post['supplier_name'],
+         'phone'        => $post['phone'],
+         'address'      => $post['address'],
+         'description'  => $post['description']
+      ];
+      $this->db->insert('supplier', $params);
+   }
+
+   public function edit($post)
+   {
+      $params = [
+         'name'         => $post['supplier_name'],
+         'phone'        => $post['phone'],
+         'address'      => $post['address'],
+         'description'  => $post['description'],
+         'updated'      => date('Y-m-d H:i:s')
+      ];
+      $this->db->where('supplier_id', $post['supplier_id']);
+      $this->db->update('supplier', $params);
+   }
+
    public function delete($where, $table)
    {
       $this->db->where($where);
