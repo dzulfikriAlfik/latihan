7b77cbc6d46bf80805a9b4335ba6eb20d489c4c9
dzulfikriAlfik <fikriallfik@gmail.com>
2020-08-14 10:20:01 +0700

    Modifikasi : 'menambahkan variable menu utk dikirim ke template'

--- kasir/application/controllers/Dashboard.php
+++ kasir/application/controllers/Dashboard.php
@@ -6,7 +6,10 @@ class Dashboard extends CI_Controller
 	public function index()
 	{
 		cek_not_login();
-		$data['aktif'] = 'dashboard';
+		$data = [
+			'aktif'	=> 'dashboard',
+			'menu'   => '',
+		];
 		$this->template->load('template', 'dashboard', $data);
 	}
 }
