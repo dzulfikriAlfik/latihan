9d4220639712c18802ec362977f7b337d1149fe3
dzulfikriAlfik <fikriallfik@gmail.com>
2020-08-14 17:17:54 +0700

    File Baru : controller[unit], model[unit], view[product/unit]

---
+++ kasir/application/controllers/Unit.php
@@ -0,0 +1,74 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Unit extends CI_Controller
+{
+
+   public function __construct()
+   {
+      parent::__construct();
+      $this->load->model('unit_model');
+      cek_not_login();
+   }
+
+   public function index()
+   {
+      $data = [
+         'row'    => $this->unit_model->get()->result_array(),
+         'aktif'  => 'unit',
+         'menu'   => 'unit',
+      ];
+      $this->template->load('template', 'product/unit/unit_data', $data);
+   }
+
+   public function add()
+   {
+      $unit = new stdClass();
+      $unit->unit_id = null;
+      $unit->name = null;
+      $data = [
+         'aktif'  => 'unit',
+         'menu'   => 'unit',
+         'page'   => 'add',
+         'row'    => $unit
+      ];
+      $this->template->load('template', 'product/unit/unit_form', $data);
+   }
+
+   public function edit($id)
+   {
+      $query = $this->unit_model->get($id);
+      if ($query->num_rows() > 0) {
+         $unit = $query->row();
+         $data = [
+            'aktif'  => 'unit',
+            'menu'   => 'unit',
+            'page'   => 'edit',
+            'row'    => $unit
+         ];
+         $this->template->load('template', 'product/unit/unit_form', $data);
+      } else {
+         pesan_alert('danger', 'Data unit tidak ditemukan', 'unit');
+      }
+   }
+
+   public function process()
+   {
+      $post = $this->input->post(null, TRUE);
+
+      if (isset($_POST['add'])) {
+         $this->unit_model->add($post);
+         pesan_alert('success', 'Data unit Berhasil ditambahkan', 'unit');
+      } else if (isset($_POST['edit'])) {
+         $this->unit_model->edit($post);
+         pesan_alert('success', 'Data unit Berhasil diupdate', 'unit');
+      }
+   }
+
+   public function delete($id)
+   {
+      $where = ['unit_id' => $id];
+      $this->unit_model->delete($where, 'p_unit');
+      pesan_alert('danger', 'Data unit Berhasil dihapus', 'unit');
+   }
+}
