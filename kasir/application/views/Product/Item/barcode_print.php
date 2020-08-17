2798e3281c9786a016dbc57cbc48ba9691fa0143
dzulfikriAlfik <fikriallfik@gmail.com>
2020-08-17 08:22:16 +0700

    Modifikasi : 'print pdf barcode'

--- kasir/application/views/Product/Item/barcode_print.php
+++ kasir/application/views/Product/Item/barcode_print.php
@@ -1,17 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Barcode Product - <?= $row->barcode; ?></title>
 </head>
 
-<body style="text-align:center">
-   <p>Barcode - <?= $row->barcode; ?></p>
+<body>
+   <h2 style="text-align:center">Barcode - <?= $row->barcode ?></h2>
    <?php
    $generator = new Picqer\Barcode\BarcodeGeneratorPNG();
-   echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode($row->barcode, $generator::TYPE_CODE_128)) .'" style="width:200px">';
+   echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode($row->barcode, $generator::TYPE_CODE_128)) . '" style="width:200px">';
    ?>
 </body>
 
