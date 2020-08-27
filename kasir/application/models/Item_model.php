80aa66f487d2388c39b7f0cf52caf2168177fb18
dzulfikriAlfik <fikriallfik@gmail.com>
2020-08-27 16:49:16 +0700

    Modifikasi : 'tambah method update_stock_out'

--- kasir/application/models/Item_model.php
+++ kasir/application/models/Item_model.php
@@ -123,4 +123,13 @@ class item_model extends CI_Model
 
       $this->db->query($sql);
    }
+
+   public function update_stock_out($data)
+   {
+      $qty  = $data['qty'];
+      $id   = $data['item_id'];
+      $sql  = "UPDATE p_item SET stock = stock - '$qty' WHERE item_id = '$id' ";
+
+      $this->db->query($sql);
+   }
 }
