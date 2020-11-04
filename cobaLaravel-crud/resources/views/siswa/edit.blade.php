e4d2187035e41c57319ece2c4e9da5cc9842167b
Dzulfikri <fikriallfik@gmail.com>
2020-11-04 10:36:49 +0700

    part-11

--- cobaLaravel-crud/resources/views/siswa/edit.blade.php
+++ cobaLaravel-crud/resources/views/siswa/edit.blade.php
@@ -3,38 +3,50 @@
 @section('title', 'Form Edit Siswa')
 
 @section('content')
-<div class="row">
-  <h1>Edit Siswa {{$siswa->nama_depan . ' ' . $siswa->nama_belakang}}</h1>
+<!-- OVERVIEW -->
+<div class="panel panel-headline">
+  <div class="panel-heading">
+    <div class="row">
+      <h2 class="text-center">Edit Siswa {{$siswa->nama_depan . ' ' . $siswa->nama_belakang}}</h2>
+    </div>
+  </div>
+  <div class="panel-body">
+    <div class="row">
+      <div class="col-md-10 col-md-offset-1">
+        <form action="/siswa/{{$siswa->id}}/update" method="POST">
+          {{csrf_field()}}
+          <div class="form-group">
+            <label for="nama_depan">Nama Depan</label>
+            <input type="text" name="nama_depan" class="form-control" id="nama_depan" placeholder="Masukan Nama Depan"
+              value="{{$siswa->nama_depan}}">
+          </div>
+          <div class="form-group">
+            <label for="nama_belakang">Nama Belakang</label>
+            <input type="text" name="nama_belakang" class="form-control" id="nama_belakang"
+              placeholder="Masukan Nama Belakang" value="{{$siswa->nama_belakang}}">
+          </div>
+          <div class="form-group">
+            <label for="jenis_kelamin">Jenis Kelamin</label>
+            <select name="jenis_kelamin" id="jenis_kelamin" class="form-control">
+              <option value="L" {{$siswa->jenis_kelamin == 'L' ? 'selected' : ''}}>Laki-laki</option>
+              <option value="P" {{$siswa->jenis_kelamin == 'P' ? 'selected' : ''}}>Perempuan</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="agama">Agama</label>
+            <input type="text" name="agama" class="form-control" id="agama" placeholder="Agama"
+              value="{{$siswa->agama}}">
+          </div>
+          <div class="form-group">
+            <label for="alamat">Alamat</label>
+            <textarea name="alamat" id="aalamat" rows="3" class="form-control">{{$siswa->alamat}}</textarea>
+          </div>
+          <a href="{{url()->previous()}}" class="btn btn-warning">Back</a>
+          <button type="submit" class="btn btn-primary">Save changes</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </div>
-
-<form action="/siswa/{{$siswa->id}}/update" method="POST">
-  {{csrf_field()}}
-  <div class="form-group">
-    <label for="nama_depan">Nama Depan</label>
-    <input type="text" name="nama_depan" class="form-control" id="nama_depan" placeholder="Masukan Nama Depan"
-      value="{{$siswa->nama_depan}}">
-  </div>
-  <div class="form-group">
-    <label for="nama_belakang">Nama Belakang</label>
-    <input type="text" name="nama_belakang" class="form-control" id="nama_belakang" placeholder="Masukan Nama Belakang"
-      value="{{$siswa->nama_belakang}}">
-  </div>
-  <div class="form-group">
-    <label for="jenis_kelamin">Jenis Kelamin</label>
-    <select name="jenis_kelamin" id="jenis_kelamin" class="form-control">
-      <option value="L" {{$siswa->jenis_kelamin == 'L' ? 'selected' : ''}}>Laki-laki</option>
-      <option value="P" {{$siswa->jenis_kelamin == 'P' ? 'selected' : ''}}>Perempuan</option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="agama">Agama</label>
-    <input type="text" name="agama" class="form-control" id="agama" placeholder="Agama" value="{{$siswa->agama}}">
-  </div>
-  <div class="form-group">
-    <label for="alamat">Alamat</label>
-    <textarea name="alamat" id="aalamat" rows="3" class="form-control">{{$siswa->alamat}}</textarea>
-  </div>
-  <a href="{{url()->previous()}}" class="btn btn-warning">Back</a>
-  <button type="submit" class="btn btn-primary">Save changes</button>
-</form>
+<!-- END OVERVIEW -->
 @endsection
--- cobaLaravel-crud/resources/views/siswa/index.blade.php
+++ cobaLaravel-crud/resources/views/siswa/index.blade.php
@@ -3,58 +3,64 @@
 @section('title', 'Daftar Siswa')
 
 @section('content')
-<div class="row">
-  <div class="col-md">
+
+@if (session('success'))
+<div class="alert alert-success alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+  <i class="fa fa-check-circle"></i> {{ session('success') }}
+</div>
+@endif
+
+<!-- OVERVIEW -->
+<div class="panel panel-headline">
+  <div class="panel-heading">
     <div class="row">
-      <div class="col-md-6">
-        <h1>Daftar Siswa</h1>
+      <div class="left">
+        <h2>Daftar Siswa</h2>
       </div>
-      <div class="col-md-6">
-        <button type="button" class="btn btn-primary btn-sm float-right" data-toggle="modal" data-target="#siswaModal">
+      <div class="right">
+        <a class="btn btn-primary btn-sm float-right" data-toggle="modal" data-target="#siswaModal">
           Tambah Siswa
-        </button>
+        </a>
       </div>
     </div>
-
-    @if (session('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-      {{ session('success') }}
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
+  </div>
+  <div class="panel-body">
+    <div class="row">
+      <div class="col-md">
+        <table class="table table-bordered table-hover table-striped">
+          <thead class="thead-dark">
+            <tr>
+              <th class="text-center">#</th>
+              <th class="text-center">NAMA</th>
+              <th class="text-center">JENIS KELAMIN</th>
+              <th class="text-center">AGAMA</th>
+              <th class="text-center">ALAMAT</th>
+              <th class="text-center">AKSI</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($data_siswa as $siswa)
+            <tr>
+              <td class="text-center">{{$loop->iteration}}</td>
+              <td>{{$siswa->nama_depan . ' ' . $siswa->nama_belakang}}</td>
+              <td class="text-center">{{$siswa->jenis_kelamin}}</td>
+              <td>{{$siswa->agama}}</td>
+              <td>{{$siswa->alamat}}</td>
+              <td class="text-center">
+                <a href="/siswa/{{$siswa->id}}/edit" class="btn btn-warning btn-xs">Edit</a>
+                <a href="/siswa/{{$siswa->id}}/delete" onclick="return confirm('Yakin?')"
+                  class="btn btn-danger btn-xs">Delete</a>
+              </td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
     </div>
-    @endif
-
-    <table class="table table-bordered table-hover table-striped">
-      <thead class="thead-dark">
-        <tr>
-          <th class="text-center">#</th>
-          <th class="text-center">NAMA</th>
-          <th class="text-center">JENIS KELAMIN</th>
-          <th class="text-center">AGAMA</th>
-          <th class="text-center">ALAMAT</th>
-          <th class="text-center">AKSI</th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach ($data_siswa as $siswa)
-        <tr>
-          <td class="text-center">{{$loop->iteration}}</td>
-          <td>{{$siswa->nama_depan . ' ' . $siswa->nama_belakang}}</td>
-          <td class="text-center">{{$siswa->jenis_kelamin}}</td>
-          <td>{{$siswa->agama}}</td>
-          <td>{{$siswa->alamat}}</td>
-          <td class="text-center">
-            <a href="/siswa/{{$siswa->id}}/edit" class="btn btn-warning btn-xs">Edit</a>
-            <a href="/siswa/{{$siswa->id}}/delete" onclick="return confirm('Yakin?')"
-              class="btn btn-danger btn-xs">Delete</a>
-          </td>
-        </tr>
-        @endforeach
-      </tbody>
-    </table>
   </div>
 </div>
+<!-- END OVERVIEW -->
 
 <!-- Modal -->
 <div class="modal fade" id="siswaModal" tabindex="-1" role="dialog" aria-labelledby="siswaModalLabel"
