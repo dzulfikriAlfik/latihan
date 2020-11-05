1d667075ce2e12453445bbe82e3c44799c69b524
Dzulfikri <fikriallfik@gmail.com>
2020-11-05 11:22:36 +0700

    mengubah nama migration + migration guru

--- cobaLaravel-crud/database/migrations/2020_11_05_041730_create_guru_table.php
+++ cobaLaravel-crud/database/migrations/2020_11_05_041730_create_guru_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMapelTabel extends Migration
+class CreateGuruTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateMapelTabel extends Migration
      */
     public function up()
     {
-        Schema::create('mapel', function (Blueprint $table) {
+        Schema::create('guru', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('kode', 255);
-            $table->string('nama', 255);
-            $table->string('semester', 45);
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ class CreateMapelTabel extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('mapel');
+        Schema::dropIfExists('guru');
     }
 }
