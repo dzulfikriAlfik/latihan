b2317bae0246e7683e539b1263b81d3ae549fadb
dzulfikriAlfik <fikriallfik@gmail.com>
2021-01-28 13:42:39 +0700

    latihan code igniter 4

--- ci4app/app/Config/Routes.php
+++ ci4app/app/Config/Routes.php
@@ -1,13 +1,12 @@
-<?php
-
-namespace Config;
+<?php namespace Config;
 
 // Create a new instance of our RouteCollection class.
 $routes = Services::routes();
 
 // Load the system's routing file first, so that the app and ENVIRONMENT
 // can override as needed.
-if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
+if (file_exists(SYSTEMPATH . 'Config/Routes.php'))
+{
 	require SYSTEMPATH . 'Config/Routes.php';
 }
 
@@ -31,17 +30,8 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Pages::index');
-// $routes->get('/coba', 'Coba::index');
-// $routes->get('/coba/about', 'Coba::about');
-// $routes->get('/coba/(:alpha)/(:num)', 'Coba::about/$1/$2');
-$routes->get('/komik/create', 'Komik::create');
-$routes->get('/komik/(:segment)', 'Komik::detail/$1');
-/*placeholder nya ada 
- - alpha : alphabet
- - num : number
- - any : apapun
- - alphanum : alphabet dan number kecuali karakter
+$routes->get('/', 'Home::index');
+
 /**
  * --------------------------------------------------------------------
  * Additional Routing
@@ -55,6 +45,7 @@ $routes->get('/komik/(:segment)', 'Komik::detail/$1');
  * You will have access to the $routes object within that file without
  * needing to reload it.
  */
-if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
+if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php'))
+{
 	require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
 }
