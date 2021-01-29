15c49635f91eaf3dd0082925f9a40ea46f35019c
dzulfikriAlfik <fikriallfik@gmail.com>
2021-01-29 15:32:50 +0700

    video ke 11

--- ci4app/app/Controllers/Komik.php
+++ ci4app/app/Controllers/Komik.php
@@ -45,13 +45,46 @@ class Komik extends BaseController
    public function create()
    {
       $data = [
-         'title' => 'Form Tambah Data Komik'
+         'title'      => 'Form Tambah Data Komik',
+         'validation' => \Config\Services::validation()
       ];
       return view('komik/create', $data);
    }
 
    public function save()
    {
+      // Validasi Input
+      if (!$this->validate([
+         'judul' => [
+            'rules'  => 'required|is_unique[komik.judul]',
+            'errors' => [
+               'required'  => '{field} harus diisi',
+               'is_unique' => '{field} komik sudah ada'
+            ]
+         ],
+         'penulis' => [
+            'rules'  => 'required',
+            'errors' => [
+               'required'  => '{field} harus diisi'
+            ]
+         ],
+         'penerbit' => [
+            'rules'  => 'required',
+            'errors' => [
+               'required'  => '{field} harus diisi',
+            ]
+         ],
+         'sampul' => [
+            'rules'  => 'required',
+            'errors' => [
+               'required'  => '{field} harus diisi'
+            ]
+         ],
+      ])) {
+         $validation = \Config\Services::validation();
+         return redirect()->to('/komik/create')->withInput()->with('validation', $validation);
+      }
+
       $slug = url_title($this->request->getVar('judul'), '-', true);
       $this->komikModel->save([
          'judul'    => $this->request->getVar('judul'),
--- ci4app/app/Views/komik/create.php
+++ ci4app/app/Views/komik/create.php
@@ -6,7 +6,10 @@
       <div class="col">
          <div class="d-flex justify-content-between">
             <h2>Form Tambah Data Komik</h2>
-            <a href="/komik" class="btn btn-sm btn-primary align-self-center">Back</a>
+            <div>
+               <a href="" class="btn btn-sm btn-warning align-self-center">Refresh</a>
+               <a href="/komik" class="btn btn-sm btn-primary align-self-center">Back</a>
+            </div>
          </div>
       </div>
    </div>
@@ -17,25 +20,37 @@
             <div class="form-group row">
                <label for="judul" class="col-sm-2 col-form-label">Judul</label>
                <div class="col-sm-10">
-                  <input type="text" class="form-control" id="judul" placeholder="Masukan Judul Komik" name="judul" autofocus>
+                  <input type="text" class="form-control <?= ($validation->hasError('judul')) ? 'is-invalid' : ''; ?>" id="judul" placeholder="Masukan Judul Komik" name="judul" autofocus value="<?= old('judul'); ?>">
+                  <div id="validationServer03Feedback" class="invalid-feedback">
+                     <?= $validation->getError('judul'); ?>
+                  </div>
                </div>
             </div>
             <div class="form-group row">
                <label for="penulis" class="col-sm-2 col-form-label">Penulis</label>
                <div class="col-sm-10">
-                  <input type="text" class="form-control" id="penulis" placeholder="Masukan Nama Penulis" name="penulis">
+                  <input type="text" class="form-control <?= ($validation->hasError('penulis')) ? 'is-invalid' : ''; ?>" id="penulis" placeholder="Masukan Nama Penulis" name="penulis" value="<?= old('penulis'); ?>">
+                  <div id="validationServer03Feedback" class="invalid-feedback">
+                     <?= $validation->getError('penulis'); ?>
+                  </div>
                </div>
             </div>
             <div class="form-group row">
                <label for="penerbit" class="col-sm-2 col-form-label">Penerbit</label>
                <div class="col-sm-10">
-                  <input type="text" class="form-control" id="penerbit" placeholder="Masukan Nama penerbit" name="penerbit">
+                  <input type="text" class="form-control <?= ($validation->hasError('penerbit')) ? 'is-invalid' : ''; ?>" id="penerbit" placeholder="Masukan Nama penerbit" name="penerbit" value="<?= old('penerbit'); ?>">
+                  <div id="validationServer03Feedback" class="invalid-feedback">
+                     <?= $validation->getError('penerbit'); ?>
+                  </div>
                </div>
             </div>
             <div class="form-group row">
                <label for="sampul" class="col-sm-2 col-form-label">Sampul</label>
                <div class="col-sm-10">
-                  <input type="text" class="form-control" id="sampul" placeholder="Masukan Nama sampul" name="sampul">
+                  <input type="text" class="form-control <?= ($validation->hasError('sampul')) ? 'is-invalid' : ''; ?>" id="sampul" placeholder="Masukan Nama sampul" name="sampul" value="<?= old('sampul'); ?>">
+                  <div id="validationServer03Feedback" class="invalid-feedback">
+                     <?= $validation->getError('sampul'); ?>
+                  </div>
                </div>
             </div>
             <div class="form-group row mt-4">
