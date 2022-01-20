ace3fdaaf1605beb6814d0833b92f37a60fbd3f7
dzulfikriAlfik <fikriallfik@gmail.com>
2022-01-20 15:51:40 +0700

    update

--- phpmvc/app/controllers/About.php
+++ phpmvc/app/controllers/About.php
@@ -2,22 +2,22 @@
 
 class About extends Controller
 {
-    public function index($nama = 'Dzulfikri', $pekerjaan = 'Programmer', $umur = 23)
-    {
-        $data['nama'] = $nama;
-        $data['pekerjaan'] = $pekerjaan;
-        $data['umur'] = $umur;
-        $data['judul'] = 'About';
-        $this->view('templates/header', $data);
-        $this->view('about/index', $data);
-        $this->view('templates/footer');
-    }
+   public function index($nama = 'Dzulfikri', $pekerjaan = 'Programmer', $umur = 23)
+   {
+      $data['nama'] = $nama;
+      $data['pekerjaan'] = $pekerjaan;
+      $data['umur'] = $umur;
+      $data['judul'] = 'About';
+      $this->view('templates/header', $data);
+      $this->view('about/index', $data);
+      $this->view('templates/footer');
+   }
 
-    public function page()
-    {
-        $data['judul'] = 'Page';
-        $this->view('templates/header', $data);
-        $this->view('about/page');
-        $this->view('templates/footer');
-    }
+   public function page()
+   {
+      $data['judul'] = 'Page';
+      $this->view('templates/header', $data);
+      $this->view('about/page');
+      $this->view('templates/footer');
+   }
 }
