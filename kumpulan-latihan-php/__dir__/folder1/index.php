827fc1cb018b68e985bd7581e568cd782d2d5ea2
dzulfikriAlfik <fikriallfik@gmail.com>
2022-05-21 16:01:14 +0700

    update

---
+++ kumpulan-latihan-php/__dir__/folder1/index.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "This location folder in " . __DIR__;
+echo "<br>";
+echo "<a href='folder1.1\index.php'>Going to folder 1.1</a><br>";
+echo "<a href='../index.php'>Going to __dir__</a>";
---
+++ kumpulan-latihan-php/__dir__/index.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "This location folder in " . __DIR__;
+echo "<br>";
+echo "<a href='folder1/index.php'>Going to folder 1</a><br>";
+echo "<a href='folder1/folder1.1/index.php'>Going to folder 1.1</a>";
