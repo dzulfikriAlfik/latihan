0a77d9be2f332e3568efc88124fcc9f149177daa
dzulfikriAlfik <fikriallfik@gmail.com>
2022-07-30 11:23:43 +0700

    update

--- kumpulan-latihan-codeigniter/wpu-login/application/config/config.php
+++ kumpulan-latihan-codeigniter/wpu-login/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/latihan/wpu-login/';
+$config['base_url'] = 'http://localhost/latihan/kumpulan-latihan-codeigniter/wpu-login/';
 
 /*
 |--------------------------------------------------------------------------
