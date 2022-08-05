9f055b9d2f62118c9a7768a4bf6a930a01665421
dzulfikriAlfik <fikriallfik@gmail.com>
2022-08-05 20:39:16 +0700

    update

--- kumpulan-latihan-javascript/01-freecodecamp-react-js/script.js
+++ kumpulan-latihan-javascript/01-freecodecamp-react-js/script.js
@@ -1,79 +1,12 @@
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Dropdown
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled">Disabled</a>
-            </li>
-          </ul>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
-      </div>
+    <nav className="navbar">
+      <img src="./react-logo.png" alt="React Logo" />
+      <ul className="nav-items">
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
     </nav>
   );
 }
@@ -81,26 +14,30 @@ function Navbar() {
 function MainContent() {
   return (
     <div>
-      <h1>I'm Learning React</h1>
-      <h2>
-        We should only have one parent element to do multiple element tags in
-        react
-      </h2>
+      <h1>Fun facts about react</h1>
+      <ul>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet consectetur.</li>
+        <li>Lorem, ipsum dolor.</li>
+        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</li>
+      </ul>
     </div>
   );
 }
 
-/** Declarative */
-ReactDOM.render(
-  <div>
-    <Navbar />
-    <MainContent />
-  </div>,
-  document.getElementById("root")
-);
+const Page = function () {
+  let name = "Dzulfikri";
+  if (name == "Dzulfikri") {
+    return (
+      <div>
+        <Navbar />
+        <MainContent />
+      </div>
+    );
+  } else {
+    return <h1>Wrong name!</h1>;
+  }
+};
 
-/** Imperative */
-// const h1 = document.createElement("h1");
-// h1.textContent = "This is an imperative way to program";
-// h1.className = "header";
-// document.getElementById("root").append(h1);
+ReactDOM.render(<Page />, document.getElementById("root"));
+// ReactDOM.render(<MainContent />, document.getElementById("root2"));
