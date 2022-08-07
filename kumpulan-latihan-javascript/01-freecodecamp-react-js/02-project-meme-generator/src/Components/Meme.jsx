f274573684611b4eabe8fb52ebe1dde57851c44c
dzulfikriAlfik <fikriallfik@gmail.com>
2022-08-07 20:12:38 +0700

    update

--- kumpulan-latihan-javascript/01-freecodecamp-react-js/02-project-meme-generator/src/Components/Meme.jsx
+++ kumpulan-latihan-javascript/01-freecodecamp-react-js/02-project-meme-generator/src/Components/Meme.jsx
@@ -4,25 +4,43 @@ import { useState } from "react";
 function Meme() {
   let memes = MemeData.data.memes;
 
-  let [imageUrl, setImageUrl] = useState("http://i.imgflip.com/1bij.jpg");
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemeImages, setAllMemeImages] = useState(memes);
 
   function getMemeImage() {
-    let rand = Math.floor(Math.random() * memes.length);
-    let newImageUrl = memes[rand].url;
-    console.log(newImageUrl);
-    setImageUrl(newImageUrl);
+    let rand = Math.floor(Math.random() * allMemeImages.length);
+    let newImageUrl = allMemeImages[rand].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: newImageUrl,
+    }));
   }
 
   return (
     <main>
       <div className="form">
-        <input type="text" className="form--input" placeholder="Top Text" />
-        <input type="text" className="form--input" placeholder="Bottom Text" />
+        <input
+          type="text"
+          id="top-text"
+          className="form--input"
+          placeholder="Top Text"
+        />
+        <input
+          type="text"
+          id="bottom-text"
+          className="form--input"
+          placeholder="Bottom Text"
+        />
         <button onClick={getMemeImage} className="form--button">
           Get a new meme image
         </button>
       </div>
-      <img src={imageUrl} className="meme--image" />
+      <img src={meme.randomImage} className="meme--image" />
     </main>
   );
 }
