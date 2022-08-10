5176445bd2e30959af904d92ba561f5645e77051
dzulfikriAlfik <fikriallfik@gmail.com>
2022-08-10 16:32:35 +0700

    update

--- kumpulan-latihan-javascript/01-freecodecamp-react-js/vite-react/src/App.jsx
+++ kumpulan-latihan-javascript/01-freecodecamp-react-js/vite-react/src/App.jsx
@@ -1,12 +1,19 @@
 import Navbar from "./Components/Navbar";
 import Main from "./Components/Main";
 import "./assets/css/Custom.css";
+import { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  function toggleDarkMode() {
+    setDarkMode((prevMode) => !prevMode);
+  }
+
   return (
     <div className="App">
-      <Navbar />
-      <Main />
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Main darkMode={darkMode} />
     </div>
   );
 }
--- kumpulan-latihan-javascript/01-freecodecamp-react-js/vite-react/src/Components/Navbar.jsx
+++ kumpulan-latihan-javascript/01-freecodecamp-react-js/vite-react/src/Components/Navbar.jsx
@@ -1,14 +1,17 @@
 import reactLogo from "../assets/react.svg";
 
-function Navbar() {
+function Navbar(props) {
   return (
-    <nav className="nav">
-      <div className="nav-left">
-        <img src={reactLogo} alt="react-logo" />
-        <h3>ReactFacts</h3>
-      </div>
-      <div className="nav-right">
-        <h4>React Course - Project 1</h4>
+    <nav className={props.darkMode ? "dark" : ""}>
+      <img className="nav--logo_icon" src={reactLogo} />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+
+      <div className="toggler">
+        <p className="toggler--light">Light</p>
+        <div className="toggler--slider" onClick={props.toggleDarkMode}>
+          <div className="toggler--slider--circle"></div>
+        </div>
+        <p className="toggler--dark">Dark</p>
       </div>
     </nav>
   );
