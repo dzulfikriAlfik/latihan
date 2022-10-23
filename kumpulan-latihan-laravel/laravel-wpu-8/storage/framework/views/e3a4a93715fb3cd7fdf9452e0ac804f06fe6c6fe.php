ae50b5ee0ce6315161b91ceaf31f7d37b299dd0a
dzulfikriAlfik <dzulfikrialfik@gmail.com>
2022-10-23 20:50:41 +0700

    update

--- kumpulan-latihan-laravel/laravel-wpu-8/storage/framework/views/e3a4a93715fb3cd7fdf9452e0ac804f06fe6c6fe.php
+++
@@ -1,7 +0,0 @@
-<?php $__env->startSection('content'); ?>
-   <div class="d-flex justify-content-between flex-md-nowrap align-items-center border-bottom mb-3 flex-wrap pt-3 pb-2">
-      <h1 class="h2">Welcome back <?php echo e(auth()->user()->name); ?></h1>
-   </div>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('dashboard.layouts.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Programming\xampp\htdocs\kumpulan-latihan-laravel\laravel-wpu-8\resources\views/dashboard/index.blade.php ENDPATH**/ ?>
