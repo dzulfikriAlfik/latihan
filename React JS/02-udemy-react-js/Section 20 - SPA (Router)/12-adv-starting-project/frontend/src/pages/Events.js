415849b50048f7dbc76a84cc378344d08f13b591
dzulfikri_alfik <dzulfikrialfik@gmail.com>
2023-02-23 08:30:22 +0700

    update

--- React JS/02-udemy-react-js/Section 20 - SPA (Router)/12-adv-starting-project/frontend/src/pages/Events.js
+++ React JS/02-udemy-react-js/Section 20 - SPA (Router)/12-adv-starting-project/frontend/src/pages/Events.js
@@ -3,9 +3,28 @@ import { useLoaderData } from 'react-router-dom'
 import EventsList from '../components/EventsList'
 
 function EventsPage() {
-  const events = useLoaderData()
+  const data = useLoaderData()
+
+  if (data.isError) {
+    return <p>{data.message}</p>
+  }
+
+  const events = data.events
 
   return <EventsList events={events} />
 }
 
 export default EventsPage
+
+export async function loader() {
+  const response = await fetch('http://localhost:8080/eventss')
+
+  if (!response.ok) {
+    throw { message: 'Could not fetch events.' }
+  } else {
+    return response
+    // const resData = await response.json()
+
+    // return resData.events
+  }
+}
