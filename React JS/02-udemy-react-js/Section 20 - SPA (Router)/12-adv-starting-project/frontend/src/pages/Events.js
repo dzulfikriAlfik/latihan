cedb126767a007b64ded386537f0f75a1d75423d
dzulfikriAlfik <fikriallfik@gmail.com>
2023-02-23 20:27:32 +0700

    Error Page

--- React JS/02-udemy-react-js/Section 20 - SPA (Router)/12-adv-starting-project/frontend/src/pages/Events.js
+++ React JS/02-udemy-react-js/Section 20 - SPA (Router)/12-adv-starting-project/frontend/src/pages/Events.js
@@ -17,14 +17,13 @@ function EventsPage() {
 export default EventsPage
 
 export async function loader() {
-  const response = await fetch('http://localhost:8080/eventss')
+  const response = await fetch('http://localhost:8080/events')
 
   if (!response.ok) {
-    throw { message: 'Could not fetch events.' }
+    throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+      status: 500,
+    })
   } else {
     return response
-    // const resData = await response.json()
-
-    // return resData.events
   }
 }
