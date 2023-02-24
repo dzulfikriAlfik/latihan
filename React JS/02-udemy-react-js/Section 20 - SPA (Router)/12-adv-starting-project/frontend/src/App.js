c207f6dfa4290e795d6b35320852ad6414536145
dzulfikriAlfik <fikriallfik@gmail.com>
2023-02-24 21:28:32 +0700

    update

--- React JS/02-udemy-react-js/Section 20 - SPA (Router)/12-adv-starting-project/frontend/src/App.js
+++ React JS/02-udemy-react-js/Section 20 - SPA (Router)/12-adv-starting-project/frontend/src/App.js
@@ -3,11 +3,12 @@ import EditEventPage from './pages/EditEvent'
 import ErrorPage from './pages/Error'
 import EventDetailPage, {
   loader as eventDetailLoader,
+  action as deleteEventAction,
 } from './pages/EventDetail'
 import EventsPage, { loader as eventsLoader } from './pages/Events'
 import EventsRootLayout from './pages/EventsRoot'
 import HomePage from './pages/Home'
-import NewEventPage from './pages/NewEvent'
+import NewEventPage, { action as newEventAction } from './pages/NewEvent'
 import RootLayout from './pages/Root'
 
 const router = createBrowserRouter([
@@ -34,12 +35,13 @@ const router = createBrowserRouter([
               {
                 index: true,
                 element: <EventDetailPage />,
+                action: deleteEventAction,
               },
               { path: 'edit', element: <EditEventPage /> },
             ],
           },
 
-          { path: 'new', element: <NewEventPage /> },
+          { path: 'new', element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
--- React JS/02-udemy-react-js/Section 20 - SPA (Router)/12-adv-starting-project/frontend/src/pages/EventDetail.js
+++ React JS/02-udemy-react-js/Section 20 - SPA (Router)/12-adv-starting-project/frontend/src/pages/EventDetail.js
@@ -1,4 +1,4 @@
-import { json, useRouteLoaderData } from 'react-router-dom'
+import { json, redirect, useRouteLoaderData } from 'react-router-dom'
 import EventItem from '../components/EventItem'
 
 export default function EventDetailPage() {
@@ -22,4 +22,18 @@ export async function loader({ params }) {
   } else {
     return response
   }
+}
+
+export async function action({ params, request }) {
+  const eventId = params.eventId
+
+  const response = await fetch(`http://localhost:8080/events/${eventId}`, {
+    method: request.method,
+  })
+
+  if (!response.ok) {
+    throw json({ message: 'Coud not delete event!' }, { status: 500 })
+  }
+
+  return redirect('/events')
 }
