e5599a9e7c16b46a17255b0ea3345c065528ac78
dzulfikri_alfik <dzulfikrialfik@gmail.com>
2023-03-23 12:53:34 +0700

    update

--- Laravel/laravel-vue-ecommerce/backend/src/views/Login.vue
+++ Laravel/laravel-vue-ecommerce/backend/src/views/Login.vue
@@ -5,28 +5,28 @@
             <div class="-space-y-px rounded-md shadow-sm">
                 <div>
                     <label for="email-address" class="sr-only"
-                        >Email address</label
+                    >Email address</label
                     >
                     <input
-                        id="email-address"
-                        name="email"
-                        type="email"
-                        autocomplete="email"
-                        required=""
-                        class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        placeholder="Email address"
+                    id="email-address"
+                    name="email"
+                    type="email"
+                    autocomplete="email"
+                    required=""
+                    class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    placeholder="Email address"
                     />
                 </div>
                 <div>
                     <label for="password" class="sr-only">Password</label>
                     <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        autocomplete="current-password"
-                        required=""
-                        class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        placeholder="Password"
+                    id="password"
+                    name="password"
+                    type="password"
+                    autocomplete="current-password"
+                    required=""
+                    class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    placeholder="Password"
                     />
                 </div>
             </div>
@@ -34,50 +34,50 @@
             <div class="flex items-center justify-between">
                 <div class="flex items-center">
                     <input
-                        id="remember-me"
-                        name="remember-me"
-                        type="checkbox"
-                        class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                    id="remember-me"
+                    name="remember-me"
+                    type="checkbox"
+                    class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
                     />
                     <label
-                        for="remember-me"
-                        class="ml-2 block text-sm text-gray-900"
-                        >Remember me</label
+                    for="remember-me"
+                    class="ml-2 block text-sm text-gray-900"
+                    >Remember me</label
                     >
                 </div>
 
                 <div class="text-sm">
                     <a
-                        href="/forgot-password"
-                        class="font-medium text-indigo-600 hover:text-indigo-500"
-                        >Forgot your password?</a
+                    href="/forgot-password"
+                    class="font-medium text-indigo-600 hover:text-indigo-500"
+                    >Forgot your password?</a
                     >
                 </div>
             </div>
 
             <div>
                 <button
-                    type="submit"
-                    class="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                type="submit"
+                class="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                    <span
-                        class="absolute inset-y-0 left-0 flex items-center pl-3"
-                    >
-                        <LockClosedIcon
-                            class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                            aria-hidden="true"
-                        />
-                    </span>
-                    Sign in
-                </button>
-            </div>
-        </form>
-    </GuestLayout>
+                <span
+                class="absolute inset-y-0 left-0 flex items-center pl-3"
+                >
+                <LockClosedIcon
+                class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                aria-hidden="true"
+                />
+            </span>
+            Sign in
+        </button>
+    </div>
+</form>
+</GuestLayout>
 </template>
 
 <script setup>
-import { LockClosedIcon } from "@heroicons/vue/20/solid";
-import GuestLayout from "../components/GuestLayout.vue";
+    import { LockClosedIcon } from "@heroicons/vue/20/solid";
+    import GuestLayout from "../components/GuestLayout.vue";
 </script>
 
 <style scoped></style>
