034e597c93d44b56b4ebd6eeeb4d277fd3976e24
DzulfikriAlkautsari <fikriallfik@gmail.com>
2023-06-13 06:47:22 +0700

    update

--- React JS/02-udemy-react-js/Section 11 - Project Food/src/components/Layout/Header.js
+++ React JS/02-udemy-react-js/Section 11 - Project Food/src/components/Layout/Header.js
@@ -1,10 +1,21 @@
 import { Fragment } from 'react'
 
-export default function Header() {
+import mealsImage from '../../assets/meals.jpg'
+import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton'
+
+const Header = (props) => {
   return (
     <Fragment>
-      <header></header>
-      <div></div>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='' />
+      </div>
     </Fragment>
   )
 }
+
+export default Header
